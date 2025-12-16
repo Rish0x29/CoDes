0a92d0557a4bfdbdbe9976b6887c301c527d5458
Rishit Das <RISHITD346@GMAIL.COM>
2025-12-16 18:10:46 +0530

    Revise Bash script for personalized greeting

--- Bash2.sh
+++ Bash2.sh
@@ -1,6 +1,15 @@
-echo "What is your name?"
-read name
-echo "how old are you"
-read age
+#!/bin/bash
 
-echo "Hello, $name, you are $age years old"
+echo "What is your name sir?"
+ read name
+echo "How can i help you ?"
+read job
+echo "It was a pleasure working for you $name Sir, I will do the $job for you "
+
+
+#####OUTPUT######
+What is your name sir?
+Rishit
+How can i help you ?
+Groceries
+It was a pleasure working for you rishit Sir, I will do the Groceries for you 
